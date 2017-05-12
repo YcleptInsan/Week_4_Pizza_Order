159caaec0bc981e06511d75fa7aa7a2cea3df57d
YcleptInsan <nickmwise@gmail.com>
2017-05-12 11:44:12 -0700

    Add front end logic for toggle of choices on click of title.

--- js/scripts.js
+++ js/scripts.js
@@ -8,16 +8,35 @@ $(document).ready(function(){
 		$(".form-group").toggle();
 		$(".col-md-6").toggle();
 
+		$("#toggle1").click(function(){
+			$("#userInputSize").slideToggle(1);
+		});
+
+		$("#toggle2").click(function(){
+			$("#userInputSauce").slideToggle(1);
+		});
+
+		$("#toggle3").click(function(){
+			$("#userInputCrust").slideToggle(1);
+		});
+
+		$("#toggle4").click(function(){
+			$("#userInputVeggieTopping").slideToggle(1);
+		});
+
+		$("#toggle5").click(function(){
+			$("#userInputMeatTopping").slideToggle(1);
+		});
+
 		$("#button1").submit(function(){
 			var pizzaSize = $("#userInputSize").val();
 			var pizzaSauce = $("#userInputSauce").val();
 			var pizzaCrust = $("#userInputCrust").val();
 			var pizzaAllToppings = $("input:checkbox[name=topping]:checked").each(function(){
-				var checkToppings = $(this).val();
+			var checkToppings = $(this).val();
 
 				//next do something with this var that has the state of which checkbox is checked
 				$('#complete-pizza').append(checkToppings + "<br>");
-
 			});
 		});
 	});
