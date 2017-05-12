045c6a4f910bf77987c2aca528394a2da0c18737
YcleptInsan <nickmwise@gmail.com>
2017-05-12 11:05:15 -0700

    Add CSS styling to page text align center.Add Event listener to show pizza making.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,24 @@
+// BACK END LOGIC BELOW THIS POINT
+
+
+// FRONT END LOGIC BELOW THIS POINT
 $(document).ready(function(){
 	$("#blueLink").click(function(event){
 		event.preventDefault();
-		$(".form-group").show();
-		$(".col-md-6").show();
+		$(".form-group").toggle();
+		$(".col-md-6").toggle();
+
+		$("#button1").submit(function(){
+			var pizzaSize = $("#userInputSize").val();
+			var pizzaSauce = $("#userInputSauce").val();
+			var pizzaCrust = $("#userInputCrust").val();
+			var pizzaAllToppings = $("input:checkbox[name=topping]:checked").each(function(){
+				var checkToppings = $(this).val();
+
+				//next do something with this var that has the state of which checkbox is checked
+				$('#complete-pizza').append(checkToppings + "<br>");
+
+			});
+		});
 	});
 });
