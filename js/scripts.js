1e718747e42aa42817ee3045b5f33532ca0b4f9c
YcleptInsan <nickmwise@gmail.com>
2017-05-14 17:01:17 -0700

    Reformat Backend logic to get user input and function for acalculating price total.

--- js/scripts.js
+++ js/scripts.js
@@ -5,26 +5,27 @@ function pizza(pizzaSize, pizzaCrust, pizzaSauce, pizzaCheckedToppings, total){
 	this.pizzaSauce = pizzaSauce;
 	this.pizzaCheckedToppings = pizzaCheckedToppings;
 	this.total = total;
+
 }
 pizza.prototype.sentence = function(){
 
 	return "You bought an" + " " + this.pizzaSize + " " + this.pizzaCrust + " " + " Style pizza with " + this.pizzaSauce + " " + "Sauce and the following toppings." + " " + this.pizzaCheckedToppings + " " + "Your total is: " + this.total;
 
 }
+function calcCost(pizzaSize, pizzaCheckedToppings, total) {
+	if(this.pizzaSize === "XL") {
+		this.total = 24.95;
+	} else if(this.pizzaSize === "Large") {
+		this.total = 19.99;
+	} else if(this.pizzaSize === "Medium") {
+		this.total = 16.00;
+	} else if(this.pizzaSize === "Small") {
+		this.total = 14.00;
+	} else {
+		return this.pizzaSize + this.total;
+	}
+}
 
-function totalCost(pizzaCheckedToppings, sizeCost) {
-	var toppingCost = pizzaCheckedToppings.length * 2.00;
-		if(sizeCost = "XL"){
-			sizeCost = 19.00;
-		} else if(sizeCost = "Large"){
-			sizeCost = 16.00;
-		} else if(sizeCost = "Medium"){
-			sizeCost = 14.00 ;
-		} else if(sizeCost = "small"){
-			sizeCost = 9.00;
-		}
-		 return (toppingCost + sizeCost).toFixed(2);
-		}
 
 // FRONT END LOGIC BELOW THIS POINT
 $(document).ready(function(){
@@ -60,18 +61,22 @@ $(document).ready(function(){
 			event.preventDefault();
 			$("#complete-pizza").show();
 			$(".col-md-6").show();
-				var pizzaSize = $("#userInputSize").val();
-				var pizzaSauce = $("#userInputSauce").val();
-				var pizzaCrust = $("#userInputCrust").val();
-				var pizzaCheckedToppings = [];
+				this.pizzaSize = $("#userInputSize").val();
+				this.pizzaSauce = $("#userInputSauce").val();
+				this.pizzaCrust = $("#userInputCrust").val();
+				this.pizzaCheckedToppings = [];
+				//trying to pass string to global varible
+				// console.log(pizzaCheckedToppings);
 
 			$("input:checkbox[name=topping]:checked").each(function() {
 				var topping = $(this).val();
-					pizzaCheckedToppings.push(topping);
+					this.pizzaCheckedToppings.push(topping);
+					console.log(topping);
+					console.log(typeof(topping));
 				});
 
-				var myPizza = new pizza(pizzaSize, pizzaCrust, pizzaSauce, pizzaCheckedToppings, total);
-				$("#complete-pizza").append("<p>" + myPizza + "</p>");
+				// var myPizza = new pizza(pizzaSize, pizzaCrust, pizzaSauce, pizzaCheckedToppings, total);
+				// $("#complete-pizza").append("<p>" + myPizza + "</p>");
 
 
 
